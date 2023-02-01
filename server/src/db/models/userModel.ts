2d1da25bf6e8ade5f9c6b8ebe1880443377c37cb
DaDaYa <you@example.com>
2023-02-01 19:59:34 +0300

    update db config

--- server/src/db/models/userModel.ts
+++ server/src/db/models/userModel.ts
@@ -4,10 +4,14 @@ import { IUser, UserRole } from '../../core/types';
 const UserSchema = new Schema<IUser>({
   username: {
     type: String,
-    unique: true
+    unique: true,
+    minlength: 5,
   },
   //TODO: don't store password as string
-  password: String,
+  password: {
+    type: String,
+    minlength: 8,
+  },
   role: {
     type: String,
     enum: UserRole
